211ff8987947450babdae0540400a8f043fc2ae5
Arnold Sujan Katru <96995931+ASKHOPE@users.noreply.github.com>
2025-03-29 00:25:52 +0530

    littel updates and safety

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,7 @@
     <h2 class="text-xl">About Wall Eve</h2>
 
 <p>Wall Eve is my latest project, a dynamic image viewer designed to bring you stunning visuals optimized for your screen. We utilize the Pexels API to source a vast and constantly updating collection of high-quality images, ensuring a consistently fresh and exciting browsing experience.</p>
-
+<br>
 <h2 class="text-xl">Key Features</h2>
 <ul>
     <li><strong>Responsive Design:</strong> Images adapt perfectly to your screen size, guaranteeing optimal viewing on any device.</li>
@@ -11,7 +11,7 @@
     <li><strong>Intuitive Navigation:</strong> Effortlessly explore the image collection using simple "Next" and "Previous" navigation buttons.</li>
     <li><strong>Pexels API Integration:</strong> Powered by the Pexels API, Wall Eve provides a constantly refreshing stream of beautiful, high-resolution images.</li>
 </ul>
-
+<br>
 <h2 class="text-xl">How to Use</h2>
 <p>Using Wall Eve is simple!  Just use the "Next" and "Previous" buttons to browse the available images.  The images will automatically adjust to fit your screen's dimensions.</p>
 
